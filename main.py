c2e146c9bdcb9012d3e5fc7f186fce5fcd4cc560
Chris Wood <chris314159@gmail.com>
2013-09-27 09:26:51 -1000

    Setting up a settings file and templates.

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from flask import Flask
 from flask import render_template
+from settings import appname 
 
-app = Flask(__name__)
+app = Flask(appname)
 
 @app.route('/')
 def main():
     return 'in main!' 
 
 @app.route('/results', methods=['GET', 'POST'])
-def display_results(result_len=100, search_type='default_method'):
-    results = range(len)
-    if search_type == 'default_method':
-        return render_template('display_results.html', method_name=search_type, res_list=results)
-    else:
+def display_results(search_type='default_method'):
+    error_msg = ''
+    results = range(100)
+    if search_type != 'default_method': #temp
         error_msg = 'Invalid or missing search type' 
+    return render_template('display_results.html', method_name=search_type,
+                               res_list=results, error_msg = error_msg)
+    
 
 if __name__=='__main__':
     app.run(debug=True)
